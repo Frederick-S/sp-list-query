bcbf532a60f8b28c13f926c9c5ee752d3d77013f
Frederick-S <mao_xiaodan@hotmail.com>
2015-08-13 11:02:25 +0800

    Remove unnecessary function

--- SPSiteDataQuery.js
+++ SPSiteDataQuery.js
@@ -257,18 +257,5 @@
         return false;
     };
 
-    var getQueryStringParameter = function (param) {
-        var params = document.URL.split("?")[1].split("&");
-        var strParams = "";
-
-        for (var i = 0; i < params.length; i = i + 1) {
-            var singleParam = params[i].split("=");
-
-            if (singleParam[0] == param) {
-                return decodeURIComponent(singleParam[1]);
-            }
-        }
-    };
-
     window.SPSiteDataQuery = SPSiteDataQuery;
 })(window, SP);
